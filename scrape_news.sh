c4e01cafe90369200e31353fcf01af58ba06a049
segevbr <segevbr@gmail.com>
2024-06-25 22:36:50 +0300

    be quiet wget

--- scrape_news.sh
+++ scrape_news.sh
@@ -11,7 +11,7 @@ articles=$(echo "$data" | \
 process_article() {
     local article=$1
 
-    content=$(wget --no-check-certificate -O - $article 2>/dev/null)
+    content=$(wget --no-check-certificate -O -b -q - $article 2>/dev/null)
 
     amount_natanyahu=$(echo "$content" | grep -oP "Netanyahu" | wc -l)
     amount_gantz=$(echo "$content" | grep -oP "Gantz" | wc -l)
