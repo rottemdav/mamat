ee8c2036afebcdeab1d9f25164cbf7978dc251dc
segevbr <segevbr@gmail.com>
2024-06-25 22:08:00 +0300

    8 cores

--- scrape_news.sh
+++ scrape_news.sh
@@ -6,7 +6,7 @@ data=$(wget --no-check-certificate -O - $site 2>/dev/null)
 
 articles=$(echo "$data" | \
             grep -oP "https://(www.)?ynetnews.com/article/[0-9a-zA-Z]+" | \
-            sort | uniq)
+            sort --parallel=16 | uniq)
 
 process_article() {
     local article=$1
