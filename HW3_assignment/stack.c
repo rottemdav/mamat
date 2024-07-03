8cc055cc39e83c9dab26e1995e0ce9a16b4ef38d
segevbr <segevbr@gmail.com>
2024-07-03 21:16:27 +0300

    Changed syntax errors on the defention of the node
    Co-authored-by: rotttemd <rotttemd@users.noreply.github.com>

--- HW3_assignment/stack.c
+++ HW3_assignment/stack.c
@@ -3,19 +3,20 @@
 
 
 typedef struct node {
-    struct node* prev;
-    struct node* next;
-    const char *value;
+    /* Must use struct node* because Node/node* typedef is only defined outside
+       of the defenition */
+    struct node* next; 
+
+    elem_t *value;
 } Node;
 
-struct Stack{
+typedef struct Stack_t{
     Node* top;
     int max_size;
     clone_t clone;
     destroy_t destroy_t;
     print_t print;
-   // elem_t element; 
-};
+} Stack;
 
 Stack* stack_create(int max_stack_size, clone_t clone,
                     destroy_t destroy, print_t print){
@@ -23,10 +24,10 @@ Stack* stack_create(int max_stack_size, clone_t clone,
     Node *new_node;
     new_node = (Node *)malloc(sizeof(new_node));
     
-    new_node->prev = NULL;
     new_node->next = NULL;
     new_node->value = NULL;
 
+    /* Initialize a pointer to the newly created stack */
     Stack *new_stack;
     new_stack = (Stack *)malloc(sizeof(new_stack));
 
@@ -37,7 +38,7 @@ Stack* stack_create(int max_stack_size, clone_t clone,
     new_stack->print = print;
         
     if (!new_stack){
-        
+        /*return error*/
     }
 
     return new_stack;
--- HW3_assignment/stack.h
+++ HW3_assignment/stack.h
@@ -2,36 +2,36 @@
 #define STACK_H
 
 /* Pointer to an element of the stack */ 
-typedef void * elem_t; 
+typedef void *elem_t; 
 
 /* Pointers to user function to clone, destroy and print an element in stack */
 typedef elem_t (*clone_t)(elem_t e); /* Clones the element */
 typedef void (*destroy_t)(elem_t e); /* Deallocate all memory of the element */
 typedef void (*print_t)(elem_t e); /* Prints the element */
 
-typedef struct Stack Stack; /* Opaque pointer to hide implemantation */
+typedef struct Stack_t* Stack; /* Opaque pointer to hide implemantation */
 
 /* */
-stack* stack_create(int max_stack_size, clone_t clone,
+Stack* stack_create(int max_stack_size, clone_t clone,
              destroy_t destroy, print_t print);
 
 /* Return if the stack was destroyed successfully */
-int stack_destroy(stack *pointer_to_stack); 
+int stack_destroy(Stack *pointer_to_stack); 
 
 /* Return if element was pushed successfully to the stack */
-int stack_push(stack *pointer_to_stack, void* new_element);
+int stack_push(Stack *pointer_to_stack, void* new_element);
 
 /* Pops an element from the top of the stack */
-void stack_pop(stack *pointer_to_stack);
+void stack_pop(Stack *pointer_to_stack);
 
 /* Returns a pointer to the element in the top of the stack */
-void* stack_peek(stack *pointer_to_stack);
+void* stack_peek(Stack *pointer_to_stack);
 
 /* Return the stack size, return 1 in case of failiure */
-int stack_size(stack *pointer_to_stack); 
+int stack_size(Stack *pointer_to_stack); 
 
 /*Prints the stack elements in decsending order - LIFO*/
-void stack_print(stack *pointer_to_stack);
+void stack_print(Stack *pointer_to_stack);
 
 
 #endif /* STACK_H_ */
