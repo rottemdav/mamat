5caaef092986d56c46bbc54dc4a79ca01ccb7e6f
segevbr <segevbr@gmail.com>
2024-07-04 19:50:30 +0300

    Finished implementing student clone, destroy and print functions. Also finished student header file.

---
+++ HW3_assignment/student.c
@@ -0,0 +1,53 @@
+#include "student.h"
+#include <stdlib.h>
+
+typedef struct Student_t {
+    char *name;
+    int age;
+    int id;
+} student;
+
+student* student_clone(const student* student_to_clone) {
+    student* clone;
+    clone = (student *)malloc(sizeof(student));
+
+    /* Checking if memory allocation is successful */
+    if (!clone){
+        return NULL;
+    }
+
+    /* Assign all the data from the original student to the clone */
+    clone->name = student_to_clone->name;
+    clone->age = student_to_clone->age;
+    clone->id = student_to_clone->id;
+
+    return clone;
+}
+
+void student_destroy(student* student_to_destroy){
+    /* Check if the student exists */
+    if (student_to_destroy != NULL){
+        
+        /* Check if need to dislocate name memory (string needs to be free) */
+        if (student_to_destroy->name != NULL){
+            free(student_to_destroy->name);
+        }
+
+        free(student_to_destroy);
+    }
+}
+
+void student_print(student* student_to_print){
+    /* Check if the student exists */
+    if (student_to_print != NULL){
+
+        /* Check if the students name exists */
+        if (student_to_print->name != NULL){
+            printf("student name: %s, age: %d, id: %d.\n", 
+            student_to_print->name,
+            student_to_print->age,
+            student_to_print->id);    
+        }
+    }
+}
+
--- HW3_assignment/student.h
+++ HW3_assignment/student.h
@@ -1,5 +1,10 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+typedef struct Student_t student; /* Opaque pointer */
+
+student *student_clone(student* student_to_clone);
+void student_destroy(student* student_to_destroy);
+void student_print(student* student_to_print);
 
 #endif /* STUDENT_H_ */
